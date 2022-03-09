e2fbe11989fc56488809b3fc5ce14b5eef863015
tenzyns <86624297+tenzyns@users.noreply.github.com>
2022-03-09 20:17:39 +0000

    usercreation endpoint complete

--- server/api.js
+++ server/api.js
@@ -1,9 +1,36 @@
 import { Router } from "express";
 
 const router = Router();
+const users = [];
+const cohortList = ["WM3", "London10", "WM4"];
+const bcrypt = require("bcrypt");
 
-router.get("/", (_, res) => {
-	res.json({ message: "Hello, world!" });
+router.get("/users", (req, res) => {
+	res.json(users);
 });
 
+router.post("/users", async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		const newUser = {
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			username: req.body.username,
+			cohort: req.body.cohort,
+			password: hashedPassword,
+		};
+		if (users.some((user) => user.username === newUser.username)) {
+			res.status(400).send("Username already exist!");
+		} else if (cohortList.every((cohort) => cohort!== newUser.cohort)) {
+			res.status(400).send("Cohort does not exist!");
+		} else if (newUser.firstName && newUser.lastName && newUser.username && newUser.cohort && newUser.password) {
+		users.push(newUser);
+		res.status(201).send("User created.");
+		} else {
+			res.status(400).send("One or more incomplete field(s)");
+		}
+	} catch {
+		res.status(500).send();
+	}
+});
 export default router;
