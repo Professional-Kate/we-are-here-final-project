994e570a52db748d196485efd5c894b90cb01d42
tenzyns <86624297+tenzyns@users.noreply.github.com>
2022-03-14 08:24:15 +0000

    updated api with database connection, but import file is still incomplete

--- server/api.js
+++ server/api.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
+import pool from ("./db");
 
 const router = Router();
-const users = [];
-const cohortList = ["WM3", "London10", "WM4"];
 const bcrypt = require("bcrypt");
 
 router.get("/users", (req, res) => {
@@ -17,22 +16,38 @@ router.post("/signup", async (req, res) => {
 			firstName: req.body.firstName,
 			lastName: req.body.lastName,
 			username: req.body.username,
-			cohort: req.body.cohort,
+			cohortId: req.body.cohortId,
 			password: hashedPassword,
+			isVolunteer: req.body.isVolunteer,
 		};
 		//checking if username already exist
-		if (users.some((user) => user.username === newUser.username)) {
-			res.status(400).send("Username already exist!");
-			//checking if a cohort exist
-		} else if (cohortList.every((cohort) => cohort!== newUser.cohort)) {
-			res.status(400).send("Cohort does not exist!");
-			//ensuring that all fields are filled
-		} else if (newUser.firstName && newUser.lastName && newUser.username && newUser.cohort && newUser.password) {
-		users.push(newUser);
-		res.status(201).send("User created.");
-		} else {
-			res.status(400).send("One or more incomplete field(s)");
-		}
+		pool
+		.query("SELECT username FROM users WHERE username=$1", [newUser.username])
+		.then((result) => {
+			if (result.rows.length > 0) {
+				res.status(400).send("Username already exist!");
+			} else {
+				//Checking if cohort ID exist
+				pool.query("SELECT id FROM cohorts WHERE id=$1", [cohortId])
+				.then((result1) => {
+					if (result1.rows.length < 1) {
+						res.status(400).send("Cohort ID doesn't exist");
+					//ensuring that all fields are filled
+					} else if (newUser.firstName && newUser.lastName && newUser.username && newUser.cohortId && newUser.password && !newUser.isVolunteer) {
+						const query = "INSERT INTO users (first_name, last_name, pass_hash, user_name, is_volunteer, cohort_id) VALUES ($1, $2, $3, $4, $5, $6)";
+						pool
+							.query(query, [newUser.firstName, newUser.lastName, newUser.password, newUser.username, newUser.isVolunteer, newUser.cohortId])
+							.then(() => res.status(200).send("User created successfully!"))
+							.catch((error) => {
+								console.error(error);
+								res.status(500).json(error);
+							})
+					} else {
+						res.status(400).send("One or more incomplete field(s)");
+					}
+				})
+			} 
+		})
 	} catch {
 		res.status(500).send();
 	}
