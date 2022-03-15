d4bd0dd487b65e6aee2bbda9edd06f221c8abfd1
gynaa78 <gynaa78@yahoo.com>
2022-03-15 22:33:47 +0000

    user and cohorts endpoints created

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import pool from "./db";
 
 const router = Router();
 
@@ -6,4 +7,26 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
+router.get("/users", async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM users");
+    res.json(users.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+router.get("/users/cohorts", async (req, res) => {
+  try {
+    const cohorts = await pool.query("SELECT * FROM cohorts");
+    res.json(cohorts.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
 export default router;
+
+
