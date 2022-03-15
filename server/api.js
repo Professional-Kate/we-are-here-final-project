7d19a9ace2a25221dc6e1dd6914a13729b4cf171
tenzyns <86624297+tenzyns@users.noreply.github.com>
2022-03-15 16:37:09 +0000

    user password creation condition added

--- server/api.js
+++ server/api.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import pool from ("./db");
+import pool from "./db";
 
 const router = Router();
 const bcrypt = require("bcrypt");
 
 router.get("/users", (req, res) => {
-	res.json(users);
+	res.json("hello world");
 });
 
 router.post("/signup", async (req, res) => {
@@ -22,18 +22,21 @@ router.post("/signup", async (req, res) => {
 		};
 		//checking if username already exist
 		pool
-		.query("SELECT username FROM users WHERE username=$1", [newUser.username])
+		.query("SELECT user_name FROM users WHERE user_name=$1", [newUser.username])
 		.then((result) => {
 			if (result.rows.length > 0) {
 				res.status(400).send("Username already exist!");
 			} else {
 				//Checking if cohort ID exist
-				pool.query("SELECT id FROM cohorts WHERE id=$1", [cohortId])
+				pool.query("SELECT id FROM cohorts WHERE id=$1", [newUser.cohortId])
 				.then((result1) => {
 					if (result1.rows.length < 1) {
 						res.status(400).send("Cohort ID doesn't exist");
-					//ensuring that all fields are filled
-					} else if (newUser.firstName && newUser.lastName && newUser.username && newUser.cohortId && newUser.password && !newUser.isVolunteer) {
+					//ensuring password and username length is 6 or more characters
+					} else if (req.body.password.length < 6 || newUser.username.length < 6) {
+						res.status(400).send("Password or username must be of 6 or more characters!");
+						//ensuring all fields are completed
+					} else if (newUser.firstName && newUser.lastName && newUser.cohortId && newUser.isVolunteer !== "") {
 						const query = "INSERT INTO users (first_name, last_name, pass_hash, user_name, is_volunteer, cohort_id) VALUES ($1, $2, $3, $4, $5, $6)";
 						pool
 							.query(query, [newUser.firstName, newUser.lastName, newUser.password, newUser.username, newUser.isVolunteer, newUser.cohortId])
@@ -41,15 +44,17 @@ router.post("/signup", async (req, res) => {
 							.catch((error) => {
 								console.error(error);
 								res.status(500).json(error);
-							})
+							});
 					} else {
 						res.status(400).send("One or more incomplete field(s)");
 					}
-				})
-			} 
-		})
+				});
+			}
+		});
 	} catch {
-		res.status(500).send();
+		res.status(500).send("Problem with pool.query");
 	}
 });
+
+
 export default router;
