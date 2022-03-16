d28e92f6eec8c6421e3a4cfd58c1d7b8de147131
Kate <84282404+Professional-Kate@users.noreply.github.com>
2022-03-16 16:53:55 +0000

    #14-Start Page-Kate (#17)
    * compelted basic looks of the page with style 'borrowed' from CYF to make it all look the same. Still need to rework CSS to make it better
    
    * added an about me button that changes state within the Landing components, when clicked will show a quick about me about the site. Made a seperate page for this but can easily link to the already made About page if needed
    
    * updated style. Changed it to use more variables so I can keep things consistent. Need to get the team together to decide on a design system for this
    
    * updated CSS slightly. Main header is now red with a black underline, just like in the about page.
    
    * added some more comments and added ID's to things that didn't have it. Ready for PR

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
+		{/* if the user isn't already logged in then send them to the /Landing page */}
+		<Route path="/landing" element={<Landing />} />
+		<Route path="/about" element={<About />} />
 	</Routes>
 );
 
---
+++ client/src/Components/SignInList.js
@@ -0,0 +1,21 @@
+const SignInList = () => {
+  return <ul id="card-list" className="card-list">
+  <li>
+    <h2 id="new-user-heading" className="card-heading underline underline-color-red">Join We Are Here</h2>
+  </li>
+  <li>
+    {/* When clicked will send users to the Sign Up endpoint */}
+    {/* can do both of these buttons with a Link element from ReactDOM */}
+    <button id="sign-up-button" className="card-button box-shadow">Sign Up</button>
+    </li>
+  <li>
+    <h2 id="old-user-heading" className="card-heading underline underline-color-red">Already have an account?</h2>
+  </li>
+  <li>
+    {/* When clicked will send users to the Sign In endpoint */}
+    <button id="sign-in-button" className="card-button box-shadow">Sign In</button>
+  </li>
+</ul>;
+};
+
+export default SignInList;
