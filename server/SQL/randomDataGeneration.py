077dbbb1ee3a526c24f8c4522d91d80c3e6fdc4f
Professional-Kate <professionalkateg@gmail.com>
2022-03-21 22:14:15 +0000

    added some more comments and cleaned up the code

--- server/SQL/randomDataGeneration.py
+++ server/SQL/randomDataGeneration.py
@@ -12,7 +12,7 @@ AMOUNT_OF_VOLUNTEERS = math.floor(AMOUNT_OF_USERS / 3) # will always be 30% of v
 COHORTS_PER_REGION = 3 # for each region will make this many cohorts
 REGIONS = ["West Midlands", "Scotland", "London", "North West", "Rome", "Cape Town"]
 MODULES = {'Intro to Digital': 1, 'Fundamentals': 3, 'Induction and Git': 1, 'HTML/CSS': 4, 'Building a team': 1, 'JavaScript Core 1': 4, 'Communicating in the workplace': 1, 'JavaScript Core 2': 4, 'Presentation Skills': 1, 'JavaScript Core 3': 4, 'Specialization': 1, 'React': 4, 'Preparing for the Job Market': 1, 'Node.js': 3, 'Interview Skills': 1, 'Databases': 4, 'Working in Teams': 1, 'Final Projects': 4, 'Presentations and Demo Day': 1} # dictionary for each module. Key = module name, value = total weeks that module takes.
-START_WEEK = datetime.date(2022, 4, 2) # Should be the first week the course starts. > YEAR, MONTH, DAY
+START_WEEK = datetime.date(2022, 4, 2) # Should be the first week the course starts. format > YEAR, MONTH, DAY . All ints
 
 path = os.path.join('./server/SQL/data') # directory for all the SQL files
 
@@ -40,7 +40,7 @@ def make_file (table):
         [first_name, last_name] = data[i - 1].split("_") # getting first and last names seperated
         is_volunteer = False
 
-        if (i <= AMOUNT_OF_VOLUNTEERS): is_volunteer = True # 30% of people will be volunteers
+        if (i <= AMOUNT_OF_VOLUNTEERS): is_volunteer = True 
 
         # every password is "password" just to make life easier 
         # inserting data into the SQL file
@@ -57,16 +57,16 @@ def make_file (table):
 
 
     case "weeks":
-      next_week = START_WEEK
-      index = 0
+      next_week = START_WEEK # have this copied so we don't mutate START_WEEK
+      index = 0 # variable to keep track of each different module we insert 
       file.write(f"INSERT INTO {table} (week_date, start_time, end_time, module_id) \nVALUES \n")
 
       # looping through each key/value in the dict to add that many weeks to the file
       for key in MODULES:
-        index += 1
+        index += 1 # for every module increase index by one
         for i in range(1, MODULES[key] + 1):
           file.write(f"('{next_week}', '10:00 AM', '04:00 PM', {index}){';' if key == 'Presentations and Demo Day' else ','} \n")
-          next_week = next_week + datetime.timedelta(days = 7)
+          next_week = next_week + datetime.timedelta(days = 7) # make a new date object based on the current week + 7 days
 
   file.close() # close the file
 
