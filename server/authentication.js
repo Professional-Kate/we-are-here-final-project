5c6daba0b10b34a237196b9e122166fa16e7f2eb
Professional-Kate <professionalkateg@gmail.com>
2022-03-31 22:29:35 +0100

    fixed the concat, username now displays nicely

--- server/authentication.js
+++ server/authentication.js
@@ -15,7 +15,7 @@ auth.get("/class/data", authentication("volunteer"), async (req, res) => {
   const { date } = req.body; // date is sent from the frontend
   const firstQuery = "WITH weeks AS (SELECT modules.name AS module_name, weeks.week_date, weeks.start_time, weeks.end_time FROM modules INNER JOIN weeks ON weeks.module_id = modules.id), cohorts AS (SELECT cohorts.number AS cohort_number, regions.name AS region_name FROM cohorts INNER JOIN regions ON regions.id = cohorts.region_id) SELECT * FROM weeks, cohorts WHERE cohorts.region_name = $1 AND cohorts.cohort_number = $2 AND weeks.week_date = $3;";
 
-  const secondQuery = "SELECT users.id, CONCAT(users.first_name, users.last_name) AS name, volunteer_flags.clockin_time, volunteer_flags.left_early, volunteer_flags.no_webcam, volunteer_flags.low_participation, volunteer_flags.absent FROM volunteer_flags FULL OUTER JOIN users ON users.id = volunteer_flags.user_id WHERE users.cohort_id = $1 AND users.is_volunteer = false;";
+  const secondQuery = "SELECT users.id, CONCAT(users.first_name, ' ', users.last_name) AS name, volunteer_flags.clockin_time, volunteer_flags.left_early, volunteer_flags.no_webcam, volunteer_flags.low_participation, volunteer_flags.absent FROM volunteer_flags FULL OUTER JOIN users ON users.id = volunteer_flags.user_id WHERE users.cohort_id = $1 AND users.is_volunteer = false;";
 
   const { number, name } = await pool.query("SELECT cohorts.number, regions.name FROM cohorts INNER JOIN regions ON regions.id = cohorts.region_id WHERE cohorts.id = $1;", [cohort_id]).then((response) => response.rows[0]);
 
